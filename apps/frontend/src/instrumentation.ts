b722af79a000e94fb9b357e9d8054989f76591d0
SebassNoob <sebastian.ong@hotmail.com>
2025-01-10 22:23:14 +0800

    chore: reformat frontend env

--- apps/frontend/src/instrumentation.ts
+++ apps/frontend/src/instrumentation.ts
@@ -1,7 +1,22 @@
 import type { Instrumentation } from "next";
 
 export async function register() {
-	// const { status } = await query("/");
+	if (process.env.NEXT_RUNTIME === "nodejs") {
+		await import("dotenv").then((mod) => mod.config());
+		await import("zod")
+			.then((mod) => mod.z)
+			.then((z) => {
+				const envSchema = z.object({
+					NODE_ENV: z.string().default("development"),
+					NEXT_PUBLIC_API_URL_SERVER: z.string().url(),
+					NEXT_PUBLIC_API_URL_CLIENT: z.string().url(),
+				});
+				const { success, error } = envSchema.safeParse(process.env);
+				if (!success) {
+					throw new Error(JSON.stringify(error));
+				}
+			});
+	}
 }
 
 export const onRequestError: Instrumentation.onRequestError = async (error, request, context) => {
