8b45fe54f5d2fe54643f045771368d3395f02e6f
SebassNoob <sebastian.ong@hotmail.com>
2025-02-05 21:29:32 +0800

    chore: fmt

--- apps/frontend/src/lib/components/SubmitButton/SubmitButton.tsx
+++ apps/frontend/src/lib/components/SubmitButton/SubmitButton.tsx
@@ -2,7 +2,9 @@ import { Button } from "@lib/components";
 import type { SubmitButtonProps } from "./types";
 
 export function SubmitButton({ subject, ...props }: SubmitButtonProps) {
-  return <Button type="submit" color="success" {...props}>
-    Save{subject ? ` ${subject}` : ""}
-  </Button>;
+	return (
+		<Button type="submit" color="success" {...props}>
+			Save{subject ? ` ${subject}` : ""}
+		</Button>
+	);
 }
--- apps/frontend/src/lib/components/SubmitButton/types.ts
+++ apps/frontend/src/lib/components/SubmitButton/types.ts
@@ -1,5 +1,5 @@
 import type { ButtonProps } from "@lib/components";
 
 export interface SubmitButtonProps extends Omit<ButtonProps, "children" | "color" | "type"> {
-  subject?: string;
+	subject?: string;
 }
